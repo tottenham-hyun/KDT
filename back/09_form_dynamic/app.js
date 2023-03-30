9c43c90c66b3dc4c3839565408fb3cd3cdaf7afa
tottenham-hyun <hwk3988@gmail.com>
2023-03-30 23:28:09 +0900

    과제

--- back/09_form_dynamic/app.js
+++ back/09_form_dynamic/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const id = "test0";
+const pw = "123";
+
 app.set("view engine", "ejs"); //view engine 등록
 app.use("/views", express.static(__dirname + "/views")); //views 파일 설정
 
@@ -29,6 +32,28 @@ app.post("/axios", (req, res) => {
   console.log(req.body);
   res.send(req.body);
 });
+
+app.get("/practice1", (req, res) => {
+  res.render("practice1");
+});
+
+app.get("/practice_axios", (req, res) => {
+  console.log(req.query.hobby);
+  res.send(req.query);
+});
+
+app.post("/practice_post", (req, res) => {
+  if (id == req.body.id) {
+    if (pw == req.body.pw) {
+      res.send({ data: req.body, res: "success" });
+    } else {
+      res.send({ res: "err" });
+    }
+  } else {
+    res.send({ res: "err" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log("listen", PORT);
 });
