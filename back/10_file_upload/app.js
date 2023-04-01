4282f94683074aa96242721229a5572c1c0170f0
tottenham-hyun <hwk3988@gmail.com>
2023-04-01 13:54:36 +0900

    multer 실습

--- back/10_file_upload/app.js
+++ back/10_file_upload/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require("express");
 const app = express();
 const PORT = 8000;
@@ -17,12 +18,14 @@ const uploadDetail = multer({
       done(null, "uploads/");
     },
     filename(req, file, done) {
+      // req : 요청 body
       // 가정) apple.png 파일을 업로드
+
       const ext = path.extname(file.originalname); // file.originalname에서 "확장자" 추출 => png
       // path.basename(file.originalname, ext) => apple (확장자 제거한 파일이름만!!)
       // Date.now() => 현재 시간 (1680309346279)
       // => 1970년 1월 1일 0시 0분 0초를 기준으로 현재까지 경과된 밀리초
-      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+      done(null, path.basename(req.body.userid,ext)+ ext);
       // [파일명 + 현재시간.확장자] 형식으로 파일 업로드
     },
   }),
@@ -60,6 +63,42 @@ app.post("/upload", uploadDetail.single("userfile"), (req, res) => {
   // { title: '이것은 망고다!!!' } // input title 에 입력합 값
 
   res.send("upload 완료~!!");
+});
+
+app.post("/upload/array", uploadDetail.array("userfile"), (req, res) => {
+  console.log(req.files);
+  console.log(req.body);
+  res.send("업로드완료");
+});
+
+app.post(
+  "/upload/field",
+  uploadDetail.fields({ name: "userfile1" }, { name: "userfile2" }),
+  (req, res) => {
+    console.log(req.files);
+    console.log(req.body);
+    res.send("각각 여러 파일 업로드 완료");
+  }
+);
+
+app.get("/practice", (req, res) => {
+  res.render("practice");
+});
+
+app.get("/practice_result", (req, res) => {
+  res.render("practice_result");
+});
+
+app.post("/result", uploadDetail.single("profile"), (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+  res.render("practice_result", {
+    userid: req.body.userid,
+    pw: req.body.password,
+    username: req.body.username,
+    age: req.body.age,
+    image: req.file.path,
+  });
 });
 
 app.listen(PORT, () => {
