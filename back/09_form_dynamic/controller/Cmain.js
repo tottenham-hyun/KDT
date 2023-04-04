6879f49855aa4f7c616a36352da114efd9c67c53
tottenham-hyun <hwk3988@gmail.com>
2023-04-04 19:28:28 +0900

    수정

--- back/09_form_dynamic/controller/Cmain.js
+++ back/09_form_dynamic/controller/Cmain.js
@@ -21,12 +21,13 @@ exports.practice1 = (req, res) => {
   for (let i = 0; i < infos.length; i++) {
     if (infos[i][0] == req.body.id) {
       if (infos[i][1] == req.body.pw) {
-        res.send({ data: req.body, res: "success" });
+        let msg = infos[i][2];
+        return res.send({ data: msg, res: "success" });
       } else {
-        res.send({ res: "err" });
+        return res.send({ res: "err" });
       }
     } else {
-      res.send({ res: "err" });
+      return res.send({ res: "err" });
     }
   }
 };
